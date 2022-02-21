8aac9a58b9584d48b245bc024455325f066658a3
Eduardo Escobar <leescobarg@gmail.com>
2022-02-20 22:05:36 -0500

    Game implementation

---
+++ src/hooks/useTypeSelector.ts
@@ -0,0 +1,3 @@
+import { useSelector, TypedUseSelectorHook } from 'react-redux';
+import { State } from '../store/reducers';
+export const useTypedSelector: TypedUseSelectorHook<State> = useSelector;
